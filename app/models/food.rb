3be51aefc80f3008e401eab50b9fd6999dcff7d2
vsidhu42 <simplyxvickx@gmail.com>
2018-03-22 01:49:34 +0000

    Models have validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,10 @@
 class Food < ApplicationRecord
   has_many :food_orders
+
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :category, presence: true
+  validates :desc, presence: true
+  validates :cost, numericality: true, presence: true
+
 end
--- app/models/food_order.rb
+++ app/models/food_order.rb
@@ -1,3 +1,6 @@
 class FoodOrder < ApplicationRecord
   belongs_to :food
+
+  validates :ordnum, numericality: { only_integer: true }, presence: true
+
 end
